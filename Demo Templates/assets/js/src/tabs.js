d6d7da1065954b1b07828bac3b5ab8a13ffc7fc1
Adam Randlett <adam@randlett.net>
2017-05-24 08:56:44 -0600

    Update for loops to non ES6 syntax for IE versions.

--- Demo Templates/assets/js/src/tabs.js
+++ Demo Templates/assets/js/src/tabs.js
@@ -1,25 +1,25 @@
 class Tabs {
-    constructor (tab = ".tabs_tab",content = ".tabs_content") {
+    constructor(tab = ".tabs_tab", content = ".tabs_content") {
         this.tab = tab;
         this.content = content;
         this.setupTabEvents();
     }
 
-    setupTabEvents () {
+    setupTabEvents() {
         const $this = this;
         const tabs = document.querySelectorAll(this.tab);
         for (var i = 0; i < tabs.length; i++) {
-            tabs[i].addEventListener('click', function (evt) {
+            tabs[i].addEventListener('click', function(evt) {
                 evt.preventDefault();
                 $this.toggle(this);
-            });
+            }, false);
         }
         // for (const tab of tabs) {
         //     tab
         // }
     }
 
-    toggle (tab) {
+    toggle(tab) {
         if (tab !== null) {
 
             const content = Array.from(document.querySelectorAll(this.content));
@@ -28,19 +28,22 @@ class Tabs {
             tab.parentNode.querySelector('.is-active').classList.toggle('is-active');
             tab.classList.toggle('is-active');
 
-            for(const tabContent of content) {
-                tabContent.style.display = "none";
+            for (var i = 0; i < content.length; i++) {
+                content[i].style.display = "none";
             }
+            // for(const tabContent of content) {
+            //     tabContent.style.display = "none";
+            // }
 
             document.querySelector(containerId).style.display = "block";
         }
     }
 
-    detectHashChange(){
+    detectHashChange() {
         const $this = this;
-        window.onhashchange = function () {
-            const tab = document.querySelector("a[href='"+window.location.hash+"']");
-            if(tab !== undefined && $this !== undefined) {
+        window.onhashchange = function() {
+            const tab = document.querySelector("a[href='" + window.location.hash + "']");
+            if (tab !== undefined && $this !== undefined) {
                 $this.toggle(tab);
             }
         }
